6a8f44b5b5e0c9f02013feda3d3f5d1aa95e84d5
noam <shemeshno@gmail.com>
2021-10-28 13:13:53 +0300

    add static adapter

--- svelte.config.js
+++ svelte.config.js
@@ -2,7 +2,12 @@ import preprocess from 'svelte-preprocess';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		// hydrate the <div id="svelte"> element in src/app.html
+		adapter: adapter({
+			// default options are shown
+			pages: 'build',
+			assets: 'build',
+			fallback: null
+		}),
 		target: '#svelte',
 		vite: {
 			optimizeDeps: {

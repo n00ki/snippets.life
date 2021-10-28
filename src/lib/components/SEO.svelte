2fdbc5aebc1dacf7f75cf3fa9c454004d08d62b8
noam <shemeshno@gmail.com>
2021-10-28 19:13:54 +0300

    add SEO

---
+++ src/lib/components/SEO.svelte
@@ -0,0 +1,23 @@
+<script>
+	let title = 'snippets.life';
+	let description = 'Life, Coded';
+</script>
+
+<svelte:head>
+	<!-- Primary tags -->
+	<title>{title}</title>
+	<meta name="title" content={title} />
+	<meta name="description" content={description} />
+
+	<!-- Open Graph -->
+	<meta property="og:type" content="website" />
+	<meta property="og:url" content="https://snippets.life/" />
+	<meta property="og:title" content={title} />
+	<meta property="og:description" content={description} />
+
+	<!-- Twitter -->
+	<meta property="twitter:card" content="summary" />
+	<meta property="twitter:url" content="https://snippets.life/" />
+	<meta property="twitter:title" content={title} />
+	<meta property="twitter:description" content={description} />
+</svelte:head>
--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,6 +1,9 @@
 <script>
+	import SEO from '$lib/components/SEO.svelte';
 	import '../styles.css';
 </script>
+
+<SEO />
 
 <main class="container mx-auto sm:h-screen font-scp font-medium">
 	<div class="h-2/5" />

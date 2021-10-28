1016718ae5d1bd20a7f01aba2adaa75d145ab982
noam <shemeshno@gmail.com>
2021-10-28 18:22:53 +0300

    change to vercel adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,10 @@
-import adapter from '@sveltejs/adapter-node';
+import vercel from '@sveltejs/adapter-vercel';
 import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter({}),
+		adapter: vercel(),
 		target: '#svelte',
 		vite: {
 			optimizeDeps: {
